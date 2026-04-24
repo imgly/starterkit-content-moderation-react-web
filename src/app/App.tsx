b8a886ea4abcde68e01fd6874f1dd65a8c73d369
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-04-24 21:50:51 +0200

    Update starterkit for v1.74.0-rc.0

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,21 +8,18 @@ import { useCallback, useState } from 'react';
 
 import CreativeEditor from '@cesdk/cesdk-js/react';
 import type CreativeEditorSDK from '@cesdk/cesdk-js';
+import type { Configuration } from '@cesdk/cesdk-js';
 
 import { initContentModerationEditor } from '../imgly';
 import { resolveAssetPath } from '../imgly/resolveAssetPath';
 import { Sidebar } from './components/Sidebar';
 import './App.css';
 
-const config = {
-  userId: 'starterkit-content-moderation-editor-user',
-  ...(import.meta.env.CESDK_USE_LOCAL && {
-    baseURL: import.meta.env.VITE_CESDK_ASSETS_BASE_URL
-  }),
-  role: 'Creator' as const
-};
+interface AppProps {
+  config: Configuration;
+}
 
-export default function App() {
+export default function App({ config }: AppProps) {
   const [cesdk, setCesdk] = useState<CreativeEditorSDK | null>(null);
 
   const handleInit = useCallback(async (instance: CreativeEditorSDK) => {
